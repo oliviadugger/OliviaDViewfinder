d6f8d7656ac814aa66ff5028043dcd81ff595010
Olivia Dugger <oliviadugger@Olivias-MacBook-Air.local>
2019-06-25 14:56:48 -0500

    Iteration 2

--- OliviaDViewfinder/AddPhotoViewController.swift
+++ OliviaDViewfinder/AddPhotoViewController.swift
@@ -15,6 +15,9 @@ class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
 
     @IBOutlet weak var imageView: UIImageView!
     
+    @IBOutlet weak var captionText: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
@@ -34,6 +37,24 @@ class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
         
         present(imagePicker, animated: true, completion: nil)
 
+    }
+    
+    
+    @IBAction func savePhotoTapped(_ sender: UIButton) {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+              let photoToSave = Photos(entity: Photos.entity(), insertInto: context)
+            photoToSave.caption = captionText.text
+            
+            if let userImage = imageView.image {
+                
+                if let userImageData = userImage.pngData() {
+                    photoToSave.imageData = userImageData
+                }
+            }
+            (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+            
+              navigationController?.popViewController(animated: true)
+        }
     }
     
     
